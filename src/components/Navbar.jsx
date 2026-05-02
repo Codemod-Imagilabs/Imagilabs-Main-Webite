8f4b9d21e2e059c41785a4f07f02e0150a5fd891
frank louis <frankplouis7@gmail.com>
2026-05-02 22:07:18 +0530

    hello

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,8 @@ const Navbar = () => {
   return (
     <nav className="absolute top-0 w-full z-50 py-6 px-8 md:px-16 flex justify-between items-center bg-transparent">
       {/* Logo */}
-      <div className="flex items-center gap-3 cursor-pointer">
-        <img src={logo} alt="Imagilabs Logo" className="w-10 h-10 object-contain" />
-        <div className="flex flex-col">
-          <span className="text-white font-bold text-2xl tracking-tighter">imagilabs</span>
-          <span className="text-[10px] font-normal tracking-normal text-gray-400 mt-[-4px]">The Creative Co.</span>
-        </div>
+      <div className="flex items-center cursor-pointer">
+        <img src={logo} alt="Imagilabs Logo" className="h-12 w-auto object-contain" />
       </div>
 
       {/* Navigation Links */}
