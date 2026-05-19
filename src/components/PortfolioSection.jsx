2ad430f852de2948669b1ded1dfe2623e0eafe83
frank louis <frankplouis7@gmail.com>
2026-05-19 22:10:02 +0530

    Initial commit

--- src/components/PortfolioSection.jsx
+++ src/components/PortfolioSection.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import lilacImg from '../assets/portfolio/Lilacbysoi.jpeg';
 import testImg from '../assets/portfolio/test.jpeg';
+import taskForge from '../assets/portfolio/TaskForge.jpeg'
 
 const PortfolioSection = () => {
   const projects = [
@@ -14,7 +15,7 @@ const PortfolioSection = () => {
       title: "TaskForge App",
       category: "Production Tracker WebApp",
       description: "A heavy-duty tracking tool for streamlining complex production workflows.",
-      image: testImg
+      image: taskForge
     },
     {
       title: "Shree Maruthi Industries",
